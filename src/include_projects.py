91c66e9d609b3c8e3a882bf83ffbfa9a2f38801c
Jonathan <j.gonzalez@merytronic.com>
2019-11-25 14:35:56 +0100

    Updated include projects

--- src/include_projects.py
+++ src/include_projects.py
@@ -5,7 +5,7 @@ INCLUDE_PROJECTS_INCR = {}
 def set_include_projects(version):
 	global INCLUDE_PROJECTS_INCR
 	if constants.GLOBAL_PROJECT == "UFD":
-		if version == '1.2.5':
+		if version == '1.2.6':
 			INCLUDE_PROJECTS_INCR = {
 				'402_00_libafs_cpp': 'YES',
 				'402_00_improxy_cpp': 'YES',
